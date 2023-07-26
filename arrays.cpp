be259a3d879f406dfc7d1512b142f58728cd1297
Tom <125534032+m7baa@users.noreply.github.com>
2023-07-26 08:02:49 +0100

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int num[5] = {};
+
+void message(){
+  cout << " : Array now " <<  "\n";
+}
+
+void print(){
+	for (int i : num){
+		cout << i << " ";
+	}
+}
+
+int main(){
+
+	print();
+
+	message();
+
+	num[0] = 100;
+	num[3] = 300;
+	num[2] = -200;
+	
+    print();
+
+	num[0] = 90;
+	num[1] = 20;
+	num[4] = 50;
+
+	message();
+
+ 	print();
+
+	cout << " : Final array" <<  "\n";
+
+    /*To find out how many elements an array has, 
+	  you have to divide the size of the array by the
+	  size of the data type it contains:*/
+	int getArrayLength = sizeof(num) / sizeof(int);
+	cout << "Array length: " << getArrayLength << endl;
+
+	return 0;
+}
